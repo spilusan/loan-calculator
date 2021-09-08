e1c29e2bcdd88c13ea91db7c1f4ed6c0bf1456c5
Chris <rikoukuroi@yahoo.com>
2021-09-08 17:33:27 -0400

    v1
    finished html on app.html. began laying ground work on site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-//Call hello world
-function helloWorld() {
-    alert("hellow world");
+//Get the user inputted Loan Amout, Term, and Interest Rates. 
+function getInput() {
+    //user inputted loan amount get
+    let loan = document.getElementbyID("loan").value;
+    //user inputted term get
+    let term = document.getElementById("term").value;
+    //user inputted interest rate amount get
+    let rate = document.getElementById("rate").value;
+    
+    //Lets make sure they're integers.
+    loan = parseInt(loan);
+    term = parseInt(term);
+    rate = parseInt(rate);
+    
+    function calculateMortgage() {
+        
+    }
+
+}
+
+
+//fill the table, total principal, total interest, monthly payments and total cost areas with the data. 
+function displayNumbers() {
+
 }
