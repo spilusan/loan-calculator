b922ea21d3d51d600048b93ee65323e756c6fb90
Chris <rikoukuroi@yahoo.com>
2021-09-10 17:49:26 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,8 @@
+var sum = 0; 
+let balanceArray = [];     
+let interestArray = [];
+let ppArray = [];
+let cumulative = [];
 //Get the user inputted Loan Amout, Term, and Interest Rates. 
 function getInput() {
     //user inputted loan amount get
@@ -6,6 +11,7 @@ function getInput() {
     let term = document.getElementById("term").value;
     //user inputted interest rate amount get
     let rate = document.getElementById("rate").value;
+    
     //Lets make sure they're integers.
     loan = parseFloat(loan);
     term = parseFloat(term);
@@ -22,55 +28,102 @@ function getInput() {
     //Okay we have the numbers now I need to do several things;
     //✓ I need to print the TMP to the id "payment"
     //✓ I need to calculate and print the principal ammount to the principal-tot id. this is literally just the loan amount so use that.
-    //I need to calculate the total interest and print that. 
-    //Specifically I need to 1. Use the interest payment formula on the remaining balance and calculate it month by month.
-    //2. I need to add up all of the interest payments.
-    //3. Then I need to display it at the interest-tot id
+    //✓ I need to calculate the total interest and print that. 
+    // I need to print the total cost
+    // I need to print the amotization table. 
+
+
+    //Rounding function taken from Mozilla Developer Documentary. This resolves the errors I was getting. 
+    function round(x) {
+        return Number.parseFloat(x).toFixed(2);
+      }
     //Logic Function
     function calculateMortgage() {       
         //BEGIN GATHERING THE REMAINING BALANCE TOTALS
         //define variables I need for the for loop:
-        let balance = loan;
-        
-        let balanceArray = [];     
-        let interestArray = [];
+        let balance = loan;        
+        let interestCumulative = 0;
         //Okay lets count from month 1 to the last month:
-        for (let index = 1; index < term; index++) {
+        for (let index = 0; index < term; index++) {
 
             //and while we're counting from month 1 to the end we need to calculate the interest and balance. 
             let interest = balance * (rate/1200);
-            balance = balance - interest;
-            let interestTotal = balance - (balance + interest);
-            //push the balance to an array.
+            let pp = TMP - interest;
+            balance = balance - pp;
+            let interestTotal = (balance + interest) - balance;
+            
+            
+            
+            //push the balance to an array.            
             balanceArray.push(balance);
-            //after storing the balance we need  
+            //Total interest                      
             interestArray.push(interestTotal);
+            
+            //round then push principal payments
+            ppArray.push(pp);
+            
+
+
                                  
         }
-
-
-
-        //USE THIS CODE LATER RIGHT NOW ITS USING THE WRONG ARRAY.
-        //CHANGE THIS TO THE ARRAY THAT HOLDS SOMETHING THAT NEEDS TO BE ADDED
-        //const sum = balanceArray.reduce(add, 0);
-        //function add(accumulator, a) {
-        //    return accumulator + a;
-        //}
-        
-        
+        for (let index = 0; index < term; index++) {
+            interestCumulative += interestArray[index];
+            //push to interestCumulative
+            cumulative.push(interestCumulative);
+            
+        }
+        //This code adds together things to give us the sum.        
+        sum = interestArray.reduce(add, 0);
+        function add(accumulator, a) {
+            return accumulator + a;
+        }
+        displayNumbers();              
+        //These are to make sure things are adding up properly please ignore, remember to comment them out when done!
         console.log(balanceArray);
-        //console.log(sum);
-        
+        console.log(interestArray);
+        console.log(sum);
+        console.log(ppArray);
+        console.log(cumulative);
+
+        cumulative = cumulative.map(function(each_element){
+            return Number(each_element.toFixed(2));
+        });
+        interestArray = interestArray.map(function(each_element){
+            return Number(each_element.toFixed(2));
+        });
+        balanceArray = balanceArray.map(function(each_element){
+            return Number(each_element.toFixed(2));
+        });
+        ppArray = ppArray.map(function(each_element){
+            return Number(each_element.toFixed(2));
+        });
         
         
     }
     
+    //fill the table, total principal, total interest, monthly payments and total cost areas with the data. 
+    function displayNumbers() {
+        //Display the principal:
+        let principal = document.getElementById("loan").value;
+        document.getElementById("principal-tot").innerHTML = principal;
+        //display the total interest:
+        sum = round(sum);
+        document.getElementById("interest-tot").innerHTML = sum;
+        //display total cost
+        document.getElementById("cost-tot").innerHTML = +principal + +sum;
+        //Display amortization table. 
+        TMP = round(TMP);
+        let table = "";
+        let b = 1;
+        for (let i = 0; i < term; i++) {
+            table += `<tr>`;
+            table += `<td>${b++}</td><td>${TMP}</td><td>${ppArray[i]}</td><td>${interestArray[i]}</td><td>${cumulative[i]}</td><td>${balanceArray[i]}</td>`;
+            table += `</tr>`
+            
+        }
+        document.getElementById("results").innerHTML = table;
+
+    }
 }
 
-//fill the table, total principal, total interest, monthly payments and total cost areas with the data. 
-function displayNumbers() {
-    //Display the principal:
-    let principal = document.getElementById("loan").value;
-    document.getElementById("principal-tot").innerHTML = principal;
 
-}
