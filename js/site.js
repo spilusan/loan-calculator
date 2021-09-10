423599c09ee9204c9852092e898fdbb14606d75c
Chris <rikoukuroi@yahoo.com>
2021-09-10 18:05:59 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+//I had to declare the variables outside of the main code so everything would work consider this my :root directory.
 var sum = 0; 
 let balanceArray = [];     
 let interestArray = [];
@@ -21,19 +22,13 @@ function getInput() {
     console.log(rate);
     //math
     let TMP = loan *  (rate / 1200) / (1 - (1 + rate/1200)**(-term));
-    console.log(TMP);
+    //console.logs throughout cause its easier for me to see whats happening the debugger hates me :(
+    //console.log(TMP);
+    //Calling my functions but maybe I don't need this 
     calculateMortgage();
     displayNumbers();
 
-    //Okay we have the numbers now I need to do several things;
-    //✓ I need to print the TMP to the id "payment"
-    //✓ I need to calculate and print the principal ammount to the principal-tot id. this is literally just the loan amount so use that.
-    //✓ I need to calculate the total interest and print that. 
-    // I need to print the total cost
-    // I need to print the amotization table. 
-
-
-    //Rounding function taken from Mozilla Developer Documentary. This resolves the errors I was getting. 
+    //Rounding function taken from Mozilla Developer Documentary. This resolves the errors I was getting rounding previously.
     function round(x) {
         return Number.parseFloat(x).toFixed(2);
       }
@@ -49,23 +44,17 @@ function getInput() {
             //and while we're counting from month 1 to the end we need to calculate the interest and balance. 
             let interest = balance * (rate/1200);
             let pp = TMP - interest;
+            //Formulas taken from coder foundry project spec sheet. 
             balance = balance - pp;
-            let interestTotal = (balance + interest) - balance;
-            
-            
-            
+            let interestTotal = (balance + interest) - balance;            
             //push the balance to an array.            
             balanceArray.push(balance);
-            //Total interest                      
-            interestArray.push(interestTotal);
-            
-            //round then push principal payments
-            ppArray.push(pp);
-            
-
-
-                                 
+            //Total interest push to array.         
+            interestArray.push(interestTotal);            
+            //push principal payment to array.
+            ppArray.push(pp); 
         }
+        //This is basically a cumulative interest counter, it doesn't work unless its in a seperate for loop. 
         for (let index = 0; index < term; index++) {
             interestCumulative += interestArray[index];
             //push to interestCumulative
@@ -77,14 +66,16 @@ function getInput() {
         function add(accumulator, a) {
             return accumulator + a;
         }
+        //I need to call the display function or variables used won't work. 
         displayNumbers();              
         //These are to make sure things are adding up properly please ignore, remember to comment them out when done!
-        console.log(balanceArray);
-        console.log(interestArray);
-        console.log(sum);
-        console.log(ppArray);
-        console.log(cumulative);
+        //console.log(balanceArray);
+        //console.log(interestArray);
+        //console.log(sum);
+        //console.log(ppArray);
+        //console.log(cumulative);
 
+        //This is a bit of code thats basically the round function but using maps in order to round each entry in the arrays. I ran this 4 times for each array, not sure if this could be better.
         cumulative = cumulative.map(function(each_element){
             return Number(each_element.toFixed(2));
         });
@@ -96,11 +87,8 @@ function getInput() {
         });
         ppArray = ppArray.map(function(each_element){
             return Number(each_element.toFixed(2));
-        });
-        
-        
-    }
-    
+        });       
+    }   
     //fill the table, total principal, total interest, monthly payments and total cost areas with the data. 
     function displayNumbers() {
         //Display the principal:
